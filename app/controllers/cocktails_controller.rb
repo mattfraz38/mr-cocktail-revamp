c0d6b2db3ba3ae1f128d1859d424b0483b5d2ea9
Matt Frazier <frazier3388@yahoo.com>
2019-07-07 14:36:01 +0900

    Cocktail controller, routes, and favicon

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,31 @@
+class CocktailsController < ApplicationController
+  
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_cocktail
+  end
+
+  def cocktail_params
+  end
+end
