02fe5da8f956644cde4e7b97829d8864e5fa926b
Matt Frazier <frazier3388@yahoo.com>
2019-07-09 08:21:45 +0900

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  puts 'Cleaning database...'
+  Cocktail.destroy_all
+
+  puts 'Creating cocktails...'
+  cocktail_attributes = [
+    { name: 'White Russian' },
+    { name: 'Bellini' },
+    { name: 'Champagne Cocktail' },
+    { name: 'Irish Coffee' },
+    { name: 'Bamboo' },
+    { name: 'Gin Gin Mule' },
+    { name: 'Long Island Ice Tea' }
+  ]
+
+Cocktail.create!(cocktail_attributes)
+puts 'Cocktails added!'
