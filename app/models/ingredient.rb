2ebaf1054b62b223746f2a60297b31610e0c9c1f
Matt Frazier <frazier3388@yahoo.com>
2019-07-15 21:15:18 +0900

    add ingredients model, doses, model, migrations, and associations between cocktails, ingredients, and doses

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
-class Cocktail < ApplicationRecord
+class Ingredient < ApplicationRecord
   has_many :doses
-  has_many :ingredients, through: :doses
+  has_many :cocktails, through: :doses
   validates :name, uniqueness: :true, presence: :true
 end
